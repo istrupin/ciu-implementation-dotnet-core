1824fa47f459093a60d4d22bdc50fba9ef7dc786
Strupinskiy <IStrupin@russellinvestments.com>
2017-08-09 10:28:47 -0700

    change tests

--- implementation-test/Array/ArrayPairSumTests.cs
+++ implementation-test/Array/ArrayPairSumTests.cs
@@ -5,7 +5,6 @@ namespace implementation_test
 {
     public class ArrayPairSum_should
     {
-
         [Theory]
         [InlineData(new [] { 1, 3, 2, 2 }, 4, 2)]
         [InlineData(new [] { 1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1  }, 10, 6)]
